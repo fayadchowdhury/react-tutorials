092bfdf0a38f424996cc4bc3159aa5114f80f7b4
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-04-03 00:20:09 -0700

    Use validation rules on Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,17 @@
 import { useRef } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
   // useRef is used to get a reference to a DOM element
   // It is similar to useState but it does not cause a re-render when the value changes
   // It has to be initialized to null since React does not have access to the DOM element when the component is first rendered
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  //   const nameRef = useRef<HTMLInputElement>(null);
+  //   const ageRef = useRef<HTMLInputElement>(null);
 
   const person = {
     name: "",
@@ -14,7 +19,12 @@ const Form = () => {
   };
 
   // Also possible to use React Hook Form to grab form inputs
-  const { register, handleSubmit } = useForm();
+  // Assign interface template to useForm to give it an idea of what kind of data to expect and also extract formState errors
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   //   const handleFormSubmit = (event: React.FormEvent) => {
   //     event.preventDefault(); // Prevent the default form submission behavior
@@ -52,10 +62,21 @@ const Form = () => {
         {/* Spread the register object and register a new hook named "name" on the name input field */}
         <input
           id="name"
-          {...register("name")}
+          // Assign custom validation rules to the name input field
+          {...register("name", { required: true, minLength: 3 })}
           type="text"
           className="form-control"
         />
+        {/* Display an error message if the name input field is empty */}
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name is required</p>
+        )}
+        {/* Display an error message if the name is less than 3 characters */}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name must be at least 3 characters long
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
