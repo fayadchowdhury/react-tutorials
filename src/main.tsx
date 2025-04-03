db101edf7e88533899055b1a6bc60e261bb2795f
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-04-03 00:01:27 -0700

    Use index.css to style app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 // import './index.css' # Use bootstrap instead
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
+import "./index.css"; // Global CSS file
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
