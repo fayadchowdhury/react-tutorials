45d52813b710a997d6164dced5a6956d9f71cc93
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-04-03 00:11:19 -0700

    Use React Hook Form to handle form data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
   // useRef is used to get a reference to a DOM element
@@ -12,30 +13,62 @@ const Form = () => {
     age: 0,
   };
 
-  const handleFormSubmit = (event: React.FormEvent) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-    if (nameRef.current) {
-      person.name = nameRef.current.value;
+  // Also possible to use React Hook Form to grab form inputs
+  const { register, handleSubmit } = useForm();
+
+  //   const handleFormSubmit = (event: React.FormEvent) => {
+  //     event.preventDefault(); // Prevent the default form submission behavior
+  //     if (nameRef.current) {
+  //       person.name = nameRef.current.value;
+  //     }
+  //     if (ageRef.current) {
+  //       person.age = parseInt(ageRef.current.value);
+  //     }
+  //     console.log(person);
+  //   };
+
+  const handleFormSubmit = (data: FieldValues) => {
+    // Access the values of the form inputs using the data parameter and the registered names
+    if (data.name) {
+      person.name = data.name;
     }
-    if (ageRef.current) {
-      person.age = parseInt(ageRef.current.value);
+    if (data.age) {
+      person.age = parseInt(data.age);
     }
     console.log(person);
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
+    // <form onSubmit={handleFormSubmit}>
+    // Handle submission of the form using React Hook Form by passing the ()(data) function to handleSubmit
+    // The data parameter will contain the values (FieldValues) of the form inputs
+    // <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" ref={nameRef} type="text" className="form-control" />
+        {/* <input id="name" ref={nameRef} type="text" className="form-control" /> */}
+        {/* Spread the register object and register a new hook named "name" on the name input field */}
+        <input
+          id="name"
+          {...register("name")}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" ref={ageRef} type="number" className="form-control" />
+        {/* <input id="age" ref={ageRef} type="number" className="form-control" /> */}
+        {/* Spread the register object and register a new hook named "age" on the age input field */}
+        <input
+          id="age"
+          {...register("age")}
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
